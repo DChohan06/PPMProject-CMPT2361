a8d3f627b48b69e0cc9378d374b4ad2b822988f2
OnKiAngel <tomo@student.douglascollege.ca>
2025-04-02 09:50:59 -0700

    Windows version

--- Files/main.cpp
+++ Files/main.cpp
@@ -11,7 +11,7 @@ using namespace std;
 
 int main() {
     
-    ifstream infile("/Users/tomonkiangel/Desktop/Shahriar.ppm");
+    ifstream infile("Shahriar.ppm");
     if (!infile) {
         cerr << "Error: Failed to open input file." << endl;
         return 1;
@@ -39,42 +39,42 @@ int main() {
 
     // Test rotation (angle in radians)
     rotatedImg = Graphics::RotateImage(rotatedImg, 1.5);
-    rotatedImg.saveImageToFile("/Users/tomonkiangel/Desktop/RotatedShahriar.ppm");
+    rotatedImg.saveImageToFile("RotatedShahriar.ppm");
 
     // Test grayscale
     grayscaleImg = Graphics::MakeGrayScale(grayscaleImg);
-    grayscaleImg.saveImageToFile("/Users/tomonkiangel/Desktop/GrayShahriar.ppm");
+    grayscaleImg.saveImageToFile("GrayShahriar.ppm");
     
 
     // Test scaling
     scaledImg = Graphics::ScaleImage(scaledImg, 0.5);
-    scaledImg.saveImageToFile("/Users/tomonkiangel/Desktop/ScaledShahriar.ppm");
+    scaledImg.saveImageToFile("ScaledShahriar.ppm");
 
     // Test translation
     translatedImg = Graphics::TranslateImage(translatedImg, 50, 50);
-    translatedImg.saveImageToFile("/Users/tomonkiangel/Desktop/TranslatedShahriar.ppm");
+    translatedImg.saveImageToFile("TranslatedShahriar.ppm");
     
     //filters - Blur, Sharpen, Edge Detection, Emboss, and one custom-designed filter
     
     //Test blurring
     blurredImg = Graphics::ApplyFilter(blurredImg, "blur");
-    blurredImg.saveImageToFile("/Users/tomonkiangel/Desktop/BlurredShahriar.ppm");
+    blurredImg.saveImageToFile("BlurredShahriar.ppm");
     
     //Test Sharpen
     sharpenedImg = Graphics::ApplyFilter(sharpenedImg, "sharpen");
-    sharpenedImg.saveImageToFile("/Users/tomonkiangel/Desktop/SharpenedShahriar.ppm");
+    sharpenedImg.saveImageToFile("SharpenedShahriar.ppm");
     
     //Test Edge Detection
     edgedImg = Graphics::ApplyFilter(edgedImg, "edgeDetected");
-    edgedImg.saveImageToFile("/Users/tomonkiangel/Desktop/EdgedShahriar.ppm");
+    edgedImg.saveImageToFile("EdgedShahriar.ppm");
     
     //Test Emboss
     embossedImg = Graphics::ApplyFilter(embossedImg, "emboss");
-    embossedImg.saveImageToFile("/Users/tomonkiangel/Desktop/EmbossedShahriar.ppm");
+    embossedImg.saveImageToFile("EmbossedShahriar.ppm");
     
     //Custom
     customedImg = Graphics::ApplyFilter(customedImg, "highContrast");
-    customedImg.saveImageToFile("/Users/tomonkiangel/Desktop/HighContrastShahriar.ppm");
+    customedImg.saveImageToFile("HighContrastShahriar.ppm");
 
     return 0;
     
