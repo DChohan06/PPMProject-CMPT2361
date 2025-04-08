da1c180f93416c4bb157a82ce98c833dc1e255ee
onkiAshley <tomo@student.douglascollege.ca>
2025-04-08 01:03:12 -0700

    Use heap instead of stack

--- Files/main.cpp
+++ Files/main.cpp
@@ -2,18 +2,6 @@
 // //  main.cpp
 // //  PPMProject
 
-// #include <iostream>
-// #include "Graphics.h"
-// int main(int argc, const char * argv[]) {
-//     // insert code here...
-//     std::cout << "Hello, World!\n";
-//     ifstream infile("Shahriar.ppm");
-//     PPM img=PPM(infile);
-//     PPM rotatedImage=Graphics::RotateImage(img,3.14);
-//     rotatedImage.saveImageToFile("RotatedShahriar.ppm");
-//     ~rotatedImage;
-//     return 0;
-// }
 #include <iostream>
 #include <fstream>
 #include "Graphics.h"
@@ -31,62 +19,75 @@ int main() {
 
     PPM img(infile);
     infile.close();
-  
+    
     // Check if the image was loaded correctly
     cout << "Image Loaded - Width: " << img.GetWidth() << ", Height: " << img.GetHeight() << endl;
 
-    // Create separate copies for each transformation
-    PPM rotatedImg (img);      // Copy constructor
-    PPM grayscaleImg (img);    // Copy constructor
-    PPM scaledImg (img);       // Copy constructor
-    PPM translatedImg (img);   // Copy constructor
+    // Create separate copies for each transformation in heap
+    PPM *rotatedImg = new PPM(img);
+    PPM *grayscaleImg = new PPM(img);
+    PPM *scaledImg = new PPM(img);
+    PPM *translatedImg = new PPM(img);
     
     //filter containers
-    PPM blurredImg(img); // Copy constructor
-    PPM sharpenedImg(img);
-    PPM edgedImg(img);
-    PPM embossedImg(img);
-    PPM customedImg(img);
+    PPM *blurredImg = new PPM(img);
+    PPM *sharpenedImg = new PPM(img);
+    PPM *edgedImg = new PPM(img);
+    PPM *embossedImg = new PPM(img);
+    PPM *customedImg = new PPM(img);
     
 
     // Test rotation (angle in radians)
-    rotatedImg = Graphics::RotateImage(rotatedImg, 1.5);
-    rotatedImg.saveImageToFile("RotatedShahriar.ppm");
+    *rotatedImg = Graphics::RotateImage(*rotatedImg, 1.5);
+    rotatedImg->saveImageToFile("RotatedShahriar.ppm");
 
     // Test grayscale
-    grayscaleImg = Graphics::MakeGrayScale(grayscaleImg);
-    grayscaleImg.saveImageToFile("GrayShahriar.ppm");
+    *grayscaleImg = Graphics::MakeGrayScale(*grayscaleImg);
+    grayscaleImg->saveImageToFile("GrayShahriar.ppm");
     
 
     // Test scaling
-    scaledImg = Graphics::ScaleImage(scaledImg, 0.5);
-    scaledImg.saveImageToFile("ScaledShahriar.ppm");
+    *scaledImg = Graphics::ScaleImage(*scaledImg, 0.5);
+    scaledImg->saveImageToFile("ScaledShahriar.ppm");
 
     // Test translation
-    translatedImg = Graphics::TranslateImage(translatedImg, 50, 50);
-    translatedImg.saveImageToFile("TranslatedShahriar.ppm");
+    *translatedImg = Graphics::TranslateImage(*translatedImg, 50, 50);
+    translatedImg->saveImageToFile("TranslatedShahriar.ppm");
     
     //filters - Blur, Sharpen, Edge Detection, Emboss, and one custom-designed filter
     
     //Test blurring
-    blurredImg = Graphics::ApplyFilter(blurredImg, "blur");
-    blurredImg.saveImageToFile("BlurredShahriar.ppm");
+    *blurredImg = Graphics::ApplyFilter(*blurredImg, "blur");
+    blurredImg->saveImageToFile("BlurredShahriar.ppm");
     
     //Test Sharpen
-    sharpenedImg = Graphics::ApplyFilter(sharpenedImg, "sharpen");
-    sharpenedImg.saveImageToFile("SharpenedShahriar.ppm");
+    *sharpenedImg = Graphics::ApplyFilter(*sharpenedImg, "sharpen");
+    sharpenedImg->saveImageToFile("SharpenedShahriar.ppm");
     
     //Test Edge Detection
-    edgedImg = Graphics::ApplyFilter(edgedImg, "edgeDetected");
-    edgedImg.saveImageToFile("EdgedShahriar.ppm");
+    *edgedImg = Graphics::ApplyFilter(*edgedImg, "edgeDetected");
+    edgedImg->saveImageToFile("EdgedShahriar.ppm");
     
     //Test Emboss
-    embossedImg = Graphics::ApplyFilter(embossedImg, "emboss");
-    embossedImg.saveImageToFile("EmbossedShahriar.ppm");
+    *embossedImg = Graphics::ApplyFilter(*embossedImg, "emboss");
+    embossedImg->saveImageToFile("EmbossedShahriar.ppm");
     
     //Custom
-    customedImg = Graphics::ApplyFilter(customedImg, "highContrast");
-    customedImg.saveImageToFile("HighContrastShahriar.ppm");
+    *customedImg = Graphics::ApplyFilter(*customedImg, "highContrast");
+    customedImg->saveImageToFile("HighContrastShahriar.ppm");
+    
+    
+    //clear heap data
+    delete rotatedImg;
+    delete grayscaleImg;
+    delete scaledImg;
+    delete translatedImg;
+    
+    delete blurredImg;
+    delete sharpenedImg;
+    delete edgedImg;
+    delete embossedImg;
+    delete customedImg;
 
     return 0;
     
